7d07391a50e1addd07c2839203c450ee8cdb46fb
Luis <luigimonge16@gmail.com>
2023-08-26 16:31:00 +0200

    Organize imports, main guard and arguments

--- run_job.py
+++ run_job.py
@@ -1,9 +1,12 @@
+import argparse
+import random
+import os
 from azure.ai.ml import MLClient, command, Input
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml.constants import AssetTypes, InputOutputModes
 
 from dotenv import load_dotenv
-import os
+
 
 
 def get_workspace(verbose=False):
@@ -24,24 +27,31 @@ def get_workspace(verbose=False):
 
     return ml_client
 
+if __name__=='__main__':
 
-ml_client = get_workspace()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--experiment_name', default=f'fruit_classification_training')
+    parser.add_argument('--n_epochs', type=int, default=3)
+    args = parser.parse_args()
 
-dataset_path = os.environ['STORAGE_DATASET_PATH'] # path structure: azureml://datastores/<storage_account_name>/paths/<container_name>
-data_type = AssetTypes.URI_FOLDER # specifies that that data asset will be a directory, instead of a file
-mode = InputOutputModes.RO_MOUNT # only read necessary
+    ml_client = get_workspace()
 
-inputs = {
-    "input_data": Input(type=data_type, path=dataset_path, mode=mode)
-}
+    dataset_path = os.environ['STORAGE_DATASET_PATH'] # path structure: azureml://datastores/<storage_account_name>/paths/<container_name>
+    data_type = AssetTypes.URI_FOLDER # specifies that that data asset will be a directory, instead of a file
+    mode = InputOutputModes.RO_MOUNT # only read necessary
 
-command_job = command(
-    code="./",
-    command="python train.py --data_dir ${{inputs.input_data}} --n_epochs 1",
-    inputs=inputs,
-    environment="fruit_env@latest",
-    compute="cpu-cluster",
-    name=f"fruit_classification_training"
-)
+    inputs = {
+        "input_data": Input(type=data_type, path=dataset_path, mode=mode),
+        "n_epochs": args.n_epochs
+    }
 
-ml_client.jobs.create_or_update(command_job)
+    command_job = command(
+        code="./",
+        command="python train.py --data_dir ${{inputs.input_data}} --n_epochs ${{inputs.n_epochs}}", 
+        inputs=inputs,
+        environment="fruit_env@latest",
+        compute="cpu-cluster",
+        name=f"{args.experiment_name}_{args.n_epochs}_{random.randint(4000, 50000)}epochs"
+    )
+
+    ml_client.jobs.create_or_update(command_job)
