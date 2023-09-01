21feeac4f6a02093e994947e8842fb09519756bf
Luis <luigimonge16@gmail.com>
2023-09-01 19:01:13 +0200

    Define get_workspace and create_environment

--- utils/azure_utils.py
+++ utils/azure_utils.py
@@ -3,43 +3,37 @@ import os
 
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
-from azure.ai.ml.entities import AmlCompute
+from azure.ai.ml.entities import Environment, BuildContext
 
-load_dotenv()
 
-# find azureml-examples/sdk/python documentation
-# https://github.com/Azure/azureml-examples/tree/main/sdk/python
+def get_workspace(verbose=False):
 
-subscription_id = os.environ['SUBSCRIPTION_ID']
-resource_group = os.environ['RESOURCE_GROUP']
-workspace_name = os.environ['WORKSPACE_NAME']
+    load_dotenv()
 
-# Connect to workspace
-ml_client = MLClient(DefaultAzureCredential(),
-                    subscription_id,
-                    resource_group,
-                    workspace_name)
+    subscription_id = os.environ['SUBSCRIPTION_ID']
+    resource_group = os.environ['RESOURCE_GROUP']
+    workspace_name = os.environ['WORKSPACE_NAME']
+    credential = DefaultAzureCredential()
 
-# uncomment this line after providing a workspace name above
-print("Welcome to your workspace: ", ml_client.workspace_name)
-for datastore in ml_client.datastores.list():
-    print(f"These are the datastores: {datastore}\n")
+    if verbose:
+        print(f"Resource Group:  {resource_group} | Subscription: {subscription_id} | {workspace_name}")
 
-# datastore to choose: 'container_name': 'azureml-blobstore-ee259bd0-6cb1-43d5-996e-855852ad4506', 'account_name': 'azuremlluis6635064614'
-
-# create compute if not exists
-cpu_compute_target = 'cpu-cluster'
-
-try:
-    ml_client.compute.get(cpu_compute_target)
-    print("Compute: ", cpu_compute_target)
-except Exception:
-    print("Creating new cpu compute target...")
-    compute = AmlCompute(
-        name=cpu_compute_target,
-        size="STANDARD_D2_V2",
-        min_instances=0,
-        max_instances=2
+    ml_client = MLClient(
+        credential=credential, subscription_id=subscription_id, resource_group_name=resource_group, workspace_name=workspace_name,
     )
-    ml_client.compute.begin_create_or_update(compute).result()
-    print("Done!")
+
+    return ml_client
+
+def create_enviroment(ml_client, docker_context_path, name):
+        '''
+        MLClient class should be already instatiated.
+        Creates an Environment in the AzureML workspace given a docker context path and a name. 
+        '''
+        print("Beginning creation of Azure ML environment...")
+        env_docker_context = Environment(
+            build=BuildContext(path=docker_context_path),
+            name=name,
+            description="Environment created from a Docker context.",
+        )
+        ml_client.environments.create_or_update(env_docker_context)
+        print("Done creating environment!")
